9831eb8ec112943c3596a66eec48de8c47bd6e09
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-05-14 16:52:18 +0530

    adding multiple Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
+import Navbar from "./Navbar"; 
+import HomePage from "./homePage";
 
 function App() { 
-  const title ="Welcome to the new blog "; 
-  const likes =50;   
-  //const person ={name:'bhamini' , age:25}; 
-  const links= "http://www.google.com";
+  // const title ="Welcome to the new blog "; 
+  // const likes =50;   
+  // //const person ={name:'bhamini' , age:25}; 
+  // const links= "http://www.google.com";
   return ( 
 
-    <div className="App">
-     <div className="content">  
-
-        <h1>App Components</h1> 
+    <div className="App"> 
+     <Navbar/>
+        <div className="content">   
+            <HomePage/>
+            <h1>App Components</h1>
+       
+       
+       
+       
+       
+       
+       
+        {/* 
         <h2>{title}</h2> 
         <p>Liked {likes} times</p>  
         <p>{10}</p>   
         <p> {"hello , prashantha"}</p>
-        {/* <p>{person}</p> */}
+        {/* <p>{person}</p> 
         
-      <p>{ "hello , prashantha" }</p> 
+     <p>{ "hello , prashantha" }</p> 
         <p>{ [1,23,4,5,6] }</p> 
         <p>{Math.random() * 10}</p>  
-        <a href={links} >Google Site</a>   
+        <a href={links} >Google Site</a>    */} 
 
      </div>
     </div>
---
+++ src/homePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+const HomePage = () => {
+    return ( 
+        <div className="home">
+  <h2> Home Page forever</h2>
+        </div>
+      );
+}
+ 
+export default HomePage;
