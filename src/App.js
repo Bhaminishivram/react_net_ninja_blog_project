e80347ea190cb5894ec9c1e4da92c7e3f176b86a
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-05-14 16:27:43 +0530

    dynamic_values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+function App() { 
+  const title ="Welcome to the new blog "; 
+  const likes =50;   
+  //const person ={name:'bhamini' , age:25}; 
+  const links= "http://www.google.com";
+  return ( 
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="content">  
+
+        <h1>App Components</h1> 
+        <h2>{title}</h2> 
+        <p>Liked {likes} times</p>  
+        <p>{10}</p>   
+        <p> {"hello , prashantha"}</p>
+        {/* <p>{person}</p> */}
+        
+      <p>{ "hello , prashantha" }</p> 
+        <p>{ [1,23,4,5,6] }</p> 
+        <p>{Math.random() * 10}</p>  
+        <a href={links} >Google Site</a>   
+
+     </div>
     </div>
   );
 }
