8d1847c7f208c895c009cae1107981f474123d02
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-06-11 15:03:23 +0530

    on Click event

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import './App.css'; 
-import Navbar from "./Navbar"; 
+import './App.css';
+import Navbar from "./Navbar";
 import HomePage from "./homePage";
 
-function App() { 
+function App() {
   // const title ="Welcome to the new blog "; 
   // const likes =50;   
   // //const person ={name:'bhamini' , age:25}; 
   // const links= "http://www.google.com";
-  return ( 
+  return (
 
-    <div className="App"> 
-       <Navbar/>
-         <div className="content">   
-            <HomePage/>
-           
-       
-       
-       
-       
-       
-       
-       
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <HomePage />
+
+
+
+
+
+
+
+
         {/* 
         <h2>{title}</h2> 
         <p>Liked {likes} times</p>  
@@ -32,9 +32,9 @@ function App() {
         <p>{ "hello , prashantha" }</p> 
         <p>{ [1,23,4,5,6] }</p> 
         <p>{Math.random() * 10}</p>  
-        <a href={links} >Google Site</a>    */} 
+        <a href={links} >Google Site</a>    */}
 
-     </div>
+      </div>
     </div>
   );
 }
--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'; 
-const HomePage = () => {
+const HomePage = () => { 
+
+     const handleClick   = (e) => {
+         console.log("Hello , ninjas", e);
+     }
+       const handleClickAgain =(name, e) =>{
+         console.log("Hello" + name, e.target.value);
+       }
     return ( 
         <div className="home">
-  <h1> Home Page forever</h1>
+          <h1> Home Page forever</h1> 
+           <button onClick={handleClick}>Click me</button>  
+          {/* <button onClick={handleClick()}>Click me</button> */}
+          {/* <button onClick={handleClickAgain('Bhamini')}>Click me Again</button> */}  
+
+          {/* using Arrow function in onClick Event button  */}
+          {/* <button onClick={() => { 
+            handleClickAgain(" Prashantha")
+          }}>
+        Click me Again 
+          </button> */} 
+
+<button onClick={(e) => 
+            handleClickAgain(" Prashantha", e)
+          }>
+        Click me Again 
+          </button>
         </div>
       );
 }
