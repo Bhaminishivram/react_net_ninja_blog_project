876e6d51260538a0e58800e077bb5ed84c328b8b
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-06-14 14:13:03 +0530

    use Effect

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import React from 'react';
 import BlogList from './BlogList';
 
@@ -36,12 +36,16 @@ const HomePage = () => {
   const handleDelete = (id) => {
      const newBlogs = blogs.filter(blog =>blog.id !== id);  
       setBlogs(newBlogs);   
-  } 
+  }  
+  useEffect( () =>{
+    console.log('use Effect ran'); 
+    console.log(blogs);
+  });
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs !"  handleDelete={handleDelete}/>
-      {/* filter */}
-      <BlogList
+        <BlogList blogs={blogs} title="All Blogs !"  handleDelete={handleDelete}/>
+            {/* filter */}
+       <BlogList
         blogs={blogs.filter((blog) => blog.author === 'bhamini')}
         title="Bhamini Blogs"  handleDelete={handleDelete}/>
 
